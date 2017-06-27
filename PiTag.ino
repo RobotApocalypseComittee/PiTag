4226e485bddd3cd372951783555d7cbf85660d55
Joe <joe.bell34@btinternet.com>
2017-06-27 13:42:02 +0100

    Nooted

--- PiTag.ino
+++ PiTag.ino
@@ -1,11 +1,11 @@
 /*
- *  PiTag.ino
- *
- *  Created on: 23.06.17
- *  
- *  The PiTag System
- *
- */
+    PiTag.ino
+
+    Created on: 23.06.17
+
+    The PiTag System
+
+*/
 
 #include <Arduino.h>
 
@@ -18,21 +18,24 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN 4
-#define RST_PIN 5
+#define SS_PIN D2
+#define RST_PIN D1
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.       // instatiate a MFRC522 reader object.
 MFRC522::MIFARE_Key key;
 
 
 #include <Hash.h>
 
-const uint8_t LEDPIN = D0;
+const uint8_t LEDPIN = D8;
 const String TEAMNAME = "blue";
-const uint32_t LEDCOLOUR = 0x00FF00;
+const uint32_t LEDCOLOUR = 0x0000FF;
 const String ID = "noot";
 String tagName;
+String state;
+int blinkCount = 0;
+bool blinkState = false;
 
-Adafruit_NeoPixel strip;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, LEDPIN, NEO_RGB + NEO_KHZ400);
 
 ESP8266WiFiMulti WiFiMulti;
 WebSocketsClient webSocket;
@@ -40,76 +43,97 @@ WebSocketsClient webSocket;
 
 
 void notifyPointScored(String attackerId) {
-  webSocket.sendTXT("pointscore:"+attackerId+","+ID);
+  webSocket.sendTXT("pointscore:" + attackerId + "," + ID);
 }
 
 // Handle WebSocket Event
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
 
 
-    switch(type) {
-        case WStype_DISCONNECTED:
-            Serial.printf("[WSc] Disconnected!\n");
-            break;
-        case WStype_CONNECTED:
-            {
-                Serial.printf("[WSc] Connected to url: %s\n",  payload);
-                // send message to server when Connected
-                webSocket.sendTXT("addplayer:"+ID+","+"blue");
-            }
-            break;
-        case WStype_TEXT:
-            Serial.printf("[WSc] get text: %s\n", payload);
-            
-            if ((char *)payload == "gameover") {
-              strip.setPixelColor(0, 255, 255, 255);
-            }
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      state = "disconnected";
+      strip.setPixelColor(0, 255, 255, 255);
+      strip.show();
+      delay(1000);
+      strip.setPixelColor(0, 0, 0, 0);
+      strip.show();
+      delay(1000);
+      break;
+    case WStype_ERROR:
+      Serial.printf("[WSc] Error: %s\n",  payload);
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WSc] Connected to url: %s\n",  payload);
+      // send message to server when Connected
+      webSocket.sendTXT("addplayer:" + ID + "," + TEAMNAME);
+      state = "playing";
+      strip.setPixelColor(0, LEDCOLOUR);
+      strip.show();
+
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+
+      if ((char *)payload == "gameover") {
+        state = "gameover";
+        strip.setPixelColor(0, 255, 255, 255);
+        strip.show();
+      } else if ((char *)payload == "eliminated") {
+        state = "eliminated";
+
+      }
 
       // send message to server
       // webSocket.sendTXT("message here");
-            break;
-    }
+      break;
+  }
 
 }
 // Do beginning Stuffs
 void setup() {
-    // Serial.begin(921600);
-    Serial.begin(9600);
+  // Serial.begin(921600);
+  Serial.begin(9600);
 
-    Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, LEDPIN, NEO_RGB + NEO_KHZ800);
-    strip.begin();
-    strip.setPixelColor(0, 0, 0, 255);
-    strip.show(); // Initialize all pixels to 'off'
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 
-    pinMode(BUILTIN_LED, OUTPUT);
+  
+  pinMode(BUILTIN_LED, OUTPUT);
 
-    //Serial.setDebugOutput(true);
-    Serial.setDebugOutput(true);
+  Serial.println("Connecting to RFID");
+  // RFID
+  SPI.begin(); // Init SPI bus
+  Serial.println("Finished");
+  delay(100);
+  mfrc522.PCD_Init(); // Init MFRC522 card
+  Serial.println("Connected");
 
-    // RFID
-    SPI.begin();      // Init SPI bus
-    mfrc522.PCD_Init(); // Init MFRC522 card
+  Serial.println();
+  Serial.println();
+  Serial.println();
 
-    Serial.println();
-    Serial.println();
-    Serial.println();
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
 
-      for(uint8_t t = 4; t > 0; t--) {
-          Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
-          Serial.flush();
-          delay(1000);
-      }
+  Serial.println("Connecting to Wifi");
+  WiFiMulti.addAP("PITAG", "letmeinnow");
+  //WiFi.disconnect();
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.println(".");
+    delay(100);
+  }
+  Serial.println("Connected. Connecting to Websocket");
 
-    WiFiMulti.addAP("PITAG", "letmeinnow");
+  webSocket.begin("172.24.1.1", 8080);
+  //webSocket.setAuthorization("user", "Password"); // HTTP Basic Authorization
+  webSocket.onEvent(webSocketEvent);
 
-    //WiFi.disconnect();
-    while(WiFiMulti.run() != WL_CONNECTED) {
-        delay(100);
-    }
-
-    webSocket.begin("172.24.1.1", 8080);
-    //webSocket.setAuthorization("user", "Password"); // HTTP Basic Authorization
-    webSocket.onEvent(webSocketEvent);
+  Serial.println("Init Completed");
 
 }
 
@@ -126,13 +150,13 @@ bool scanCard() {
     return false;
   }
 
- 
+
   Serial.print(F("Card UID:"));
   checkCard(mfrc522.uid.uidByte, mfrc522.uid.size);
   Serial.println();
   digitalWrite(BUILTIN_LED, HIGH);
   return true;
-  
+
 }
 
 void checkCard(byte *buffer, byte bufferSize) {
@@ -147,13 +171,29 @@ void checkCard(byte *buffer, byte bufferSize) {
 }
 
 void loop() {
+  Serial.println("Checking for card");
   if (scanCard()) {
-    for (int i; i<100;i++) {
+    for (int i; i < 100; i++) {
+      Serial.println("nooting");
       delay(100);
       webSocket.loop();
     }
   }
+  if (state == "eliminated") {
+    blinkCount++;
+    if (blinkCount == 50) {
+      blinkCount = 0;
+      Serial.println("Blink");
+      if (blinkState) {
+        strip.setPixelColor(0, 0, 0, 0);
+      } else {
+        strip.setPixelColor(0, LEDCOLOUR);
+      }
+      blinkState = !blinkState;
+    }
+  }
   webSocket.loop();
+  strip.show();
 }
 
 
